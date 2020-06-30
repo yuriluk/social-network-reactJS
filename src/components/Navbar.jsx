145ed5debd5ba55eee29bffd4f57f4c5b75e47dc
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-06-30 15:39:49 +0300

    14

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import s from './Navbar.module.css'
+
 
 const Navbar = () => {
-    return <nav className='nav'>
-        <div>
+    return <nav className={s.nav}>
+        <div className={s.item}>
             <a href="#">Profile</a>
         </div>
-        <div><a href="#">Messages</a></div>
-        <div><a href="#">News</a></div>
-        <div><a href="#">Music</a></div>
-        <div><a href="#">Settings</a></div>
+        <div className={s.item}>
+            <a href="#">
+                Messages</a></div>
+        <div className={s.item}>
+            <a href="#">
+                News
+            </a>
+        </div>
+        <div className={s.item}>
+            <a href="#">
+                Music</a>
+        </div>
+        <div className={s.item}>
+            <a href="#">
+                Settings</a>
+        </div>
     </nav>
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React from 'react'
+import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://wallpapercave.com/wp/P42nRq2.jpg" alt=""/>
             </div>
             <div>
-                ava+descr
+                ava+description
             </div>
             <div>
                 My posts
                 <div>New post</div>
-                <div>post1</div>
-                <div>post2</div>
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        post1
+                    </div>
+                    <div className={s.item}>
+                        post2
+                    </div>
+                </div>
             </div>
         </div>
     )
