3dedfba9d1e6c917b59ffe400929d60a20ffc2de
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-06-30 19:37:36 +0300

    20

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,35 +4,27 @@ import {NavLink} from "react-router-dom";
 
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-        <div className={s.item}>
-            <NavLink to={'/profile'}>
-                Profile
-            </NavLink>
-        </div>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to='/profile' activeClassName={s.activeLink} > Profile </NavLink>
+            </div>
 
-        <div className={s.item}>
-            <NavLink to={'/dialogs'}>
-                Messages
-            </NavLink>
-        </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to='/dialogs' activeClassName={s.activeLink}> Messages </NavLink>
+            </div>
 
-        <div className={s.item}>
-            <NavLink to={'/news'}>
-                News
-            </NavLink>
-        </div>
-        <div className={s.item}>
-            <NavLink to={'/music'}>
-                Music
-            </NavLink>
-        </div>
-        <div className={s.item}>
-            <NavLink to={'/settings'}>
-                Settings
-            </NavLink>
-        </div>
-    </nav>
+            <div className={s.item}>
+                <NavLink to='/news' activeClassName={s.activeLink}> News </NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music' activeClassName={s.activeLink}> Music </NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' activeClassName={s.activeLink}> Settings </NavLink>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
