f62d48575f9cd10afe038669b8d692ef44c6354a
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-06-30 19:52:48 +0300

    21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,22 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Dimych</div>
+                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Roma</div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>HOw are you</div>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>Ha-Ha</div>
+                <div className={s.message}>Viu</div>
+            </div>
         </div>
     )
 }
