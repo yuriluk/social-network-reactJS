2a63c2b27298540f0ef886366d58e17e8744a489
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-06-30 16:37:12 +0300

    16

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,12 +11,12 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={'First post'} likesCount={15}/>
+                <Post message={'Second post'} likesCount={3}/>
+                <Post message={'Third post'} likesCount={7}/>
+                <Post message={'Forth post'} likesCount={5}/>
+                <Post message={'Fifth post'} likesCount={85}/>
+                <Post message={'Sixth post'} likesCount={45}/>
             </div>
         </div>
     )
