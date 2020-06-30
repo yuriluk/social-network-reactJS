4c358a8780ea0296d30fb7a1c75fb4039f950f91
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-07-01 00:18:10 +0300

    26

---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './../Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={"/dialogs/" + props.key}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+
+export default DialogItem
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,7 @@
 import React from 'react'
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
-
-const DialogItem = (props) => {
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={"/dialogs/" + props.key}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 
 const Dialogs = (props) => {
