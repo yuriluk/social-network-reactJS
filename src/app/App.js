f26459f0d66f912b6ae97c27057a175ad076757b
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-06-30 13:02:28 +0300

    13

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Profile from "../components/Profile";
+import Footer from "../components/Footer";
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Profile/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://wallpapercave.com/wp/P42nRq2.jpg" alt=""/>
+            </div>
+            <div>
+                ava+descr
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
