0008aac7d8aa5a1bda8afb8291d7f52560c3f7e2
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-07-01 12:48:26 +0300

    26

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,18 +2,9 @@ import React from 'react'
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        {id:1, message:'First post' , likesCount:15},
-        {id:2, message:'Second post' , likesCount:17},
-        {id:3, message:'Third post' , likesCount:3},
-        {id:4, message:'Forth post' , likesCount:5},
-        {id:5, message:'Fifth post' , likesCount:85},
-        {id:6, message:'Sixth post' , likesCount:75},
-    ]
-
-    let postsElements = posts
+    let postsElements = props.posts
         .map(el => (<Post key={el.id} message={el.message} likesCount={el.likesCount}/>))
 
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,20 @@ import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
+
+    let posts = [
+        {id:1, message:'First post' , likesCount:15},
+        {id:2, message:'Second post' , likesCount:17},
+        {id:3, message:'Third post' , likesCount:3},
+        {id:4, message:'Forth post' , likesCount:5},
+        {id:5, message:'Fifth post' , likesCount:85},
+        {id:6, message:'Sixth post' , likesCount:75},
+    ]
+
     return (
         <>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={posts}/>
         </>
     )
 }
