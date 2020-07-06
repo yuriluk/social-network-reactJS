9006d8dd88061aae853f29893ab9d202571f461b
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-07-06 15:20:06 +0300

    60 - React JS - withRouter, props.match.params

--- src/app/App.js
+++ src/app/App.js
@@ -18,7 +18,7 @@ const App = () => {
             <NavbarContainer/>
 
             <div className='app-wrapper-content'>
-                <Route path='/profile' render={() => <ProfileContainer/>}/>
+                <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
                 <Route path='/dialogs' render={() => <DialogsContainer/>}/>
                 <Route path='/news' component={News}/>
                 <Route path='/music' component={Music}/>
